487ff30b38bb7293701dab5e6d91bfdccede04c7
Dragos Cosmin <dragos.cosmin1@gmail.com>
2020-03-02 16:44:28 +0200

    DataBaseUserService implementat si functional except for backup

--- src/main/java/ro/jademy/contactlist/service/DataBaseUserService.java
+++ src/main/java/ro/jademy/contactlist/service/DataBaseUserService.java
@@ -167,77 +167,70 @@ public class DataBaseUserService implements UserService {
                 String jobTitle = result.getString("job_title");
                 boolean isFavorite = result.getBoolean("is_favorite");
 
+                Statement phoneStatement = userConnection.createStatement();
+                String phoneQuery = "SELECT phone_type, country_code, area_code, phonenumber FROM " + dataBaseName + ".phone_numbers\n" +
+                        "WHERE user_id=" + userId + ";";
+                ResultSet phoneResult = phoneStatement.executeQuery(phoneQuery);
 
+                Map<String, PhoneNumber> phoneNumbers = new HashMap<>();
 
-                    Statement phoneStatement = userConnection.createStatement();
-                    String phoneQuery = "SELECT phone_type, country_code, area_code, phonenumber FROM " + dataBaseName + ".phone_numbers\n" +
-                            "WHERE user_id=" + userId + ";";
-                    ResultSet phoneResult = phoneStatement.executeQuery(phoneQuery);
+                while (phoneResult.next()) {
+                    String phoneType = phoneResult.getString("phone_type");
+                    String countryCode = phoneResult.getString("country_code");
+                    String areaCode = phoneResult.getString("area_code").substring(1);
+                    String phoneNumber = phoneResult.getString("phonenumber");
+                    phoneNumbers.put(phoneType, new PhoneNumber(countryCode, areaCode, phoneNumber));
+                }
+                phonesList.add(phoneNumbers);
 
-                    Map<String, PhoneNumber> phoneNumbers = new HashMap<>();
+                Statement addressConnectionStatement = userConnection.createStatement();
+                String addressQuerry = "SELECT adress_type, street_name, street_no,apart_no,floor,zip_code,city,country, companies.company_name FROM adresses\n" +
+                        "LEFT JOIN companies\n" +
+                        "ON adresses.user_id=companies.user_id\n" +
+                        "WHERE adresses.user_id=" + userId + ";";
 
-                    while (phoneResult.next()) {
-                        String phoneType = phoneResult.getString("phone_type");
-                        String countryCode = phoneResult.getString("country_code");
-                        String areaCode = phoneResult.getString("area_code").substring(1);
-                        String phoneNumber = phoneResult.getString("phonenumber");
-                        phoneNumbers.put(phoneType, new PhoneNumber(countryCode, areaCode, phoneNumber));
+                ResultSet addressResult = addressConnectionStatement.executeQuery(addressQuerry);
+                Address homeAdress = new Address();
+                Address workAdress = new Address();
+                Company company = new Company();
+
+                while (addressResult.next()) {
+                    String adresstype = addressResult.getString("adress_type");
+                    String streetName = addressResult.getString("street_name");
+                    Integer streetNumber = addressResult.getInt("street_no");
+                    Integer apartNumber = addressResult.getInt("apart_no");
+                    String floor = addressResult.getString("floor");
+                    String zipCode = addressResult.getString("zip_code");
+                    String city = addressResult.getString("city");
+                    String country = addressResult.getString("country");
+                    String compName = addressResult.getString("companies.company_name");
+
+                    if (adresstype.equalsIgnoreCase("home")) {
+
+                        homeAdress.setStreetName(streetName);
+                        homeAdress.setStreetNumber(streetNumber);
+                        homeAdress.setApartmentNumber(apartNumber);
+                        homeAdress.setFloor(floor);
+                        homeAdress.setZipCode(zipCode);
+                        homeAdress.setCity(city);
+                        homeAdress.setCountry(country);
+
+                    } else {
+
+                        workAdress.setStreetName(streetName);
+                        workAdress.setStreetNumber(streetNumber);
+                        workAdress.setApartmentNumber(apartNumber);
+                        workAdress.setFloor(floor);
+                        workAdress.setZipCode(zipCode);
+                        workAdress.setCity(city);
+                        workAdress.setCountry(country);
+                        company.setName(compName);
+                        company.setAddress(workAdress);
+
                     }
-                    phonesList.add(phoneNumbers);
-
-
-
-
-                    Statement addressConnectionStatement = userConnection.createStatement();
-                    String addressQuerry = "SELECT adress_type, street_name, street_no,apart_no,floor,zip_code,city,country, companies.company_name FROM adresses\n" +
-                            "LEFT JOIN companies\n" +
-                            "ON adresses.user_id=companies.user_id\n" +
-                            "WHERE adresses.user_id=" + userId + ";";
-
-                    ResultSet addressResult = addressConnectionStatement.executeQuery(addressQuerry);
-                    Address homeAdress = new Address();
-                    Address workAdress = new Address();
-                    Company company = new Company();
-
-                    while (addressResult.next()) {
-                        String adresstype = addressResult.getString("adress_type");
-                        String streetName = addressResult.getString("street_name");
-                        Integer streetNumber = addressResult.getInt("street_no");
-                        Integer apartNumber = addressResult.getInt("apart_no");
-                        String floor = addressResult.getString("floor");
-                        String zipCode = addressResult.getString("zip_code");
-                        String city = addressResult.getString("city");
-                        String country = addressResult.getString("country");
-                        String compName = addressResult.getString("companies.company_name");
-
-                        if (adresstype.equalsIgnoreCase("home")) {
-
-                            homeAdress.setStreetName(streetName);
-                            homeAdress.setStreetNumber(streetNumber);
-                            homeAdress.setApartmentNumber(apartNumber);
-                            homeAdress.setFloor(floor);
-                            homeAdress.setZipCode(zipCode);
-                            homeAdress.setCity(city);
-                            homeAdress.setCountry(country);
-
-                        } else {
-
-                            workAdress.setStreetName(streetName);
-                            workAdress.setStreetNumber(streetNumber);
-                            workAdress.setApartmentNumber(apartNumber);
-                            workAdress.setFloor(floor);
-                            workAdress.setZipCode(zipCode);
-                            workAdress.setCity(city);
-                            workAdress.setCountry(country);
-                            company.setName(compName);
-                            company.setAddress(workAdress);
-
-                        }
-                    }
-                    homeAdressList.add(homeAdress);
-                    companyList.add(company);
-
-
+                }
+                homeAdressList.add(homeAdress);
+                companyList.add(company);
 
 
                 User user = new User(firstName, lastName, email, age, phonesList.get(phonesList.size() - 1), homeAdressList.get(homeAdressList.size() - 1), jobTitle, companyList.get(companyList.size() - 1), isFavorite);
@@ -298,27 +291,27 @@ public class DataBaseUserService implements UserService {
 
     }
 
-    private void deleteUserFromDB(User contact){
+    private void deleteUserFromDB(User contact) {
         try {
-            Connection connection=getConnection();
-            Statement statement=connection.createStatement();
-            String querry="DELETE FROM companies\n"+
-                    "WHERE user_id="+contact.getId()+";";
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            String querry = "DELETE FROM companies\n" +
+                    "WHERE user_id=" + contact.getId() + ";";
             statement.executeUpdate(querry);
 
-            querry="DELETE FROM adresses\n"+
-                    "WHERE user_id="+contact.getId()+";";
+            querry = "DELETE FROM adresses\n" +
+                    "WHERE user_id=" + contact.getId() + ";";
             statement.executeUpdate(querry);
 
-            querry="DELETE FROM phone_numbers\n"+
-                    "WHERE user_id="+contact.getId()+";";
+            querry = "DELETE FROM phone_numbers\n" +
+                    "WHERE user_id=" + contact.getId() + ";";
             statement.executeUpdate(querry);
 
-            querry="DELETE FROM users\n"+
-                    "WHERE id="+contact.getId()+";";
+            querry = "DELETE FROM users\n" +
+                    "WHERE id=" + contact.getId() + ";";
             statement.executeUpdate(querry);
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
